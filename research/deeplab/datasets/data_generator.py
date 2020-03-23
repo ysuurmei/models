a5e5bf3f7d54305e9f20f17de3abc3ffd57b5441
Youp <43464255+ysuurmei@users.noreply.github.com>
2020-03-23 14:56:55 +0000

    Update data_generator.py

--- research/deeplab/datasets/data_generator.py
+++ research/deeplab/datasets/data_generator.py
@@ -99,10 +99,21 @@ _ADE20K_INFORMATION = DatasetDescriptor(
     ignore_label=0,
 )
 
+_IMAT_FASHION_INFORMATION = DatasetDescriptor(
+    splits_to_sizes={
+        'train': 31964,  # num of samples in images/training ubuntu vim command: ':echo line('$')'
+        'val': 3602,  # num of samples in images/validation
+        'trainval': 35566
+    },
+    num_classes=5,
+    ignore_label=0,
+)
+
 _DATASETS_INFORMATION = {
     'cityscapes': _CITYSCAPES_INFORMATION,
     'pascal_voc_seg': _PASCAL_VOC_SEG_INFORMATION,
     'ade20k': _ADE20K_INFORMATION,
+    'imat_fashion': _IMAT_FASHION_INFORMATION
 }
 
 # Default file pattern of TFRecord of TensorFlow Example.
