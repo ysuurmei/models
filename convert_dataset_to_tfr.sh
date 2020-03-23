2ae3a6827a1de37b9fdad0a88b76f975f857be1f
Youp <43464255+ysuurmei@users.noreply.github.com>
2020-03-23 15:29:50 +0000

    Create convert_dataset_to_tfr.sh

---
+++ convert_dataset_to_tfr.sh
@@ -0,0 +1,16 @@
+WORK_DIR="/home/ubuntu/data_imat/"
+PQR_ROOT="${WORK_DIR}/dl_dataset"
+SEG_FOLDER="${PQR_ROOT}/SegmentationClass"
+SEMANTIC_SEG_FOLDER="${PQR_ROOT}/SegmentationClass"
+# Build TFRecords of the dataset.
+OUTPUT_DIR="${WORK_DIR}/tfrecord"
+mkdir -p "${OUTPUT_DIR}"
+IMAGE_FOLDER="${PQR_ROOT}/JPEGImages"
+LIST_FOLDER="${PQR_ROOT}/ImageSets"
+echo "Converting PQR dataset..."
+python research/deeplab/datasets/build_voc2012_data.py \
+  --image_folder="${IMAGE_FOLDER}" \
+  --semantic_segmentation_folder="${SEMANTIC_SEG_FOLDER}" \
+  --list_folder="${LIST_FOLDER}" \
+  --image_format="jpg" \
+  --output_dir="${OUTPUT_DIR}"
