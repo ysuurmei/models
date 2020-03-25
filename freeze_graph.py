93f18a1502c9640f3ed7893318cec6ffb4d62808
Youp <43464255+ysuurmei@users.noreply.github.com>
2020-03-25 14:22:30 +0000

    Update freeze_graph.py

--- freeze_graph.py
+++ freeze_graph.py
@@ -1,7 +1,7 @@
 from tensorflow.python.tools import freeze_graph
 
-freeze_graph.freeze_graph('~/data_imat/dl_dataset/PQR/exp/train_on_trainval_set/train/graph.pbtxt', "", False,
-                          '~/data_imat/dl_dataset/PQR/exp/train_on_trainval_set/train/model.ckpt-10000', "output/softmax",
+freeze_graph.freeze_graph('/home/ubuntu/data_imat/dl_dataset/PQR/exp/train_on_trainval_set/train/graph.pbtxt', "", False,
+                          '/home/ubuntu/data_imat/dl_dataset/PQR/exp/train_on_trainval_set/train/model.ckpt-10000', "output/softmax",
                            "save/restore_all", "save/Const:0",
-                           '~/data_imat/dl_dataset/PQR/exp/train_on_trainval_set/train/frozentensorflowModel.pb', True, ""
+                           '/home/ubuntu/data_imat/dl_dataset/PQR/exp/train_on_trainval_set/train/frozentensorflowModel.pb', True, ""
                          )
