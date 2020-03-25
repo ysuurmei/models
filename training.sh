669c187e71ba87fa4ac52b4692db7104de9b69bb
Youp <43464255+ysuurmei@users.noreply.github.com>
2020-03-25 14:56:23 +0000

    add graph export to training.sh

--- training.sh
+++ training.sh
@@ -35,6 +35,14 @@ python "${WORK_DIR}"/train.py \
   --train_logdir="${TRAIN_LOGDIR}" \
   --dataset_dir="${DATASET}"
 
+python deeplab/export_model.py
+  --checkpoint_path=/home/ubuntu/data_imat/dl_dataset/PQR/exp/train_on_trainval_set/train/model.ckpt-10000  \
+  --export_path=/home/ubuntu/data_imat/dl_dataset/PQR/exp/train_on_trainval_set/train/frozen_inference_graph.pb \
+  --model_variant="xception_65" \
+  --atrous_rates=6 \
+  --atrous_rates=12 \
+  --atrous_rates=18
+
 python "${WORK_DIR}"/eval.py \
 --logtostderr \
 --eval_split="val" \
