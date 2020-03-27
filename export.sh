55a2d44f861b55c1555fd594b74c5228b4329668
Youp <43464255+ysuurmei@users.noreply.github.com>
2020-03-27 16:12:31 +0000

    Update export.sh

--- export.sh
+++ export.sh
@@ -7,7 +7,7 @@ python research/deeplab/export_model.py \
   --vis_split="val"
   --checkpoint_path='/home/ubuntu/data_imat/dl_dataset/PQR/exp/train_on_trainval_set/train/model.ckpt-4343'  \
   --export_path=/home/ubuntu/data_imat/dl_dataset/PQR/exp/train_on_trainval_set/train/frozen_inference_graph.pb \
-  --dataset = "imat_fashion"
+  --dataset = "imat_fashion" \
   --model_variant="xception_65" \
   --atrous_rates=6 \
   --atrous_rates=12 \
@@ -15,5 +15,5 @@ python research/deeplab/export_model.py \
   --vis_crop_size=512 \
   --vis_crop_size=512 \
   --decoder_output_stride=4 \
-  --output_stride=16 \
+  --output_stride=16
 
