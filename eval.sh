d4408971e1123f7a3cc59139b54cf4d961891e7a
Youp <43464255+ysuurmei@users.noreply.github.com>
2020-03-27 10:22:43 +0000

    Update eval.sh

--- eval.sh
+++ eval.sh
@@ -23,7 +23,7 @@ python "${WORK_DIR}"/eval.py \
 --atrous_rates=18 \
 --output_stride=16 \
 --decoder_output_stride=4 \
---eval_crop_size=3000,3000 \
+--eval_crop_size=2999,2999 \
 --fine_tune_batch_norm=False \
 --checkpoint_dir="${TRAIN_LOGDIR}" \
 --eval_logdir="${EVAL_LOGDIR}" \
