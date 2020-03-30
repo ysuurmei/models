cd9495a1af2813d648a5ed3d38f9cd9d38431c14
Youp <43464255+ysuurmei@users.noreply.github.com>
2020-03-30 17:36:07 +0100

    Update shutdown.py

--- run/utils/shutdown.py
+++ run/utils/shutdown.py
@@ -1,4 +1,4 @@
 import boto3
 ids = ['i-0f88a515df7d24572']
-ec2 = boto3.resource('ec2')
+ec2 = boto3.resource('ec2', region_name='eu-west-2')
 ec2.instances.filter(InstanceIds = ids).stop() #for stopping an ec2 instance
