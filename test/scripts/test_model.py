3baf5076c2db9071d7b2bbb148dc670e7edf9173
Youp <43464255+ysuurmei@users.noreply.github.com>
2020-03-31 15:59:14 +0100

    update training

--- test/scripts/test_model.py
+++ test/scripts/test_model.py
@@ -1,21 +1,22 @@
 import tarfile
 import os
 from PIL import Image
+from deeplab import DeepLabModel
+
 
 def make_tarfile(output_filename, source_dir):
     with tarfile.open(output_filename, "w:gz") as tar:
         tar.add(source_dir, arcname=os.path.basename(source_dir))
 
 if __name__ == '__main__':
-    from test.scripts.deeplab import DeepLabModel
     import glob
     from line_profiler import LineProfiler
     from imgaug.augmentables.segmaps import SegmentationMapOnImage
     import numpy as np
     from progressbar import progressbar
 
-    PATH_MODEL = r'test\4k.tar.gz'
-    PATH_IMAGES = r'test\test_images'
+    PATH_MODEL = r'C:\Users\YoupSuurmeijer\Documents\models\test\models\model_v1_50000.tar.gz'
+    PATH_IMAGES = r'C:\Users\YoupSuurmeijer\Documents\models\test\test_images'
     PATH_OUTPUT = os.path.join(r'C:\Users\YoupSuurmeijer\Documents\models\test\test_output',
                   os.path.basename(PATH_MODEL).split('.')[0])
 
