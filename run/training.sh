c171f434333608855a7cd5d9fb3db7144c12aaab
Youp <43464255+ysuurmei@users.noreply.github.com>
2020-05-20 10:52:46 +0200

    Update training.sh

--- run/training.sh
+++ run/training.sh
@@ -30,7 +30,7 @@ cp run/training.sh "${DATASET_DIR}/training_settings.txt"
 # Run the python training script
 python research/deeplab/train.py \
   --logtostderr \
-  --num_clones=4 \
+  --num_clones=2 \
   --save_summaries_secs=120\
   --dataset="imat_fashion" \
   --train_split="train" \
